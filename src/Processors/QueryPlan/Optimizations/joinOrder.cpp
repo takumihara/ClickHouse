f6bfae3b965399bff6ba335a415bfbc136debd08
vdimir <vdimir@clickhouse.com>
2026-03-12 10:30:34 +0000

    upd isValidJoinOrder

--- src/Processors/QueryPlan/Optimizations/joinOrder.cpp
+++ src/Processors/QueryPlan/Optimizations/joinOrder.cpp
@@ -537,12 +537,16 @@ std::optional<JoinKind> JoinOrderOptimizer::isValidJoinOrder(const BitSet & left
     JoinKind right_join_type = JoinKind::Inner;
 
     if (auto res = check(left_mask, right_mask))
-        left_join_type = res.value();
+    {
+        left_join_type = isLeftOrFull(res.value()) ? reverseJoinKind(res.value()) : res.value();
+    }
     else
         return {};
 
     if (auto res = check(right_mask, left_mask))
-        right_join_type = reverseJoinKind(res.value());
+    {
+        right_join_type = isRightOrFull(res.value()) ? reverseJoinKind(res.value()) : res.value();
+    }
     else
         return {};
 
